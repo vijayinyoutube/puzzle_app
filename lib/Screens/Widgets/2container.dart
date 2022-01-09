eb7f732af9ff28b933b60edba1cc83a60bdda68e
Vijay <vijaythevirtue@gmail.com>
2022-01-10 00:32:01 +0530

    refactor

--- lib/Screens/Widgets/2container.dart
+++ lib/Screens/Widgets/2container.dart
@@ -7,50 +7,69 @@ class BuildContainerClass extends StatelessWidget {
   const BuildContainerClass({Key? key, required this.value}) : super(key: key);
 
   final int value;
-
   static int zeroIndex = homePageNotifier.myArray.value.indexOf(0);
   static int lastClicked = 0;
+  static int hoverIndex = 0;
+  static Color hoverColor = HexColor("#0e459c");
 
   @override
   Widget build(BuildContext context) {
-    return buildColorContainer(value, context);
+    return ValueListenableBuilder<bool>(
+        valueListenable: homePageNotifier.isHovering,
+        builder: (context, values, _) {
+          return buildColorContainer(value, context);
+        });
   }
 
   Widget buildColorContainer(int index, BuildContext context) => Padding(
         padding: const EdgeInsets.all(2.0),
-        child: GestureDetector(
-          onTap: () {
-            lastClicked = index;
-            zeroIndex = homePageNotifier.myArray.value.indexOf(0);
-
-            if (isAdjacent(index) && isNotDiagonallyOpposite(index)) {
-              homePageNotifier.updateArray(
-                  homePageNotifier.myArray.value.indexOf(index), 0);
-              homePageNotifier.updateArray(zeroIndex, index);
-            }
+        child: MouseRegion(
+          onEnter: (PointerEvent event) {
+            homePageNotifier.updateHover();
+            hoverIndex = index;
+            print("object");
           },
-          child: Container(
-            width: 100,
-            height: 100,
-            child: Center(
-                child: index != 0
-                    ? Text(
-                        index.toString(),
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 30),
-                      )
-                    : const SizedBox()),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.00),
-                // border: Border.all(
-                //   color: Colors.white,
-                //   width: 3,
-                // ),
-                color: index == 0
-                    ? Colors.transparent
-                    : lastClicked == index
-                        ? HexColor("#66b1f1")
-                        : HexColor("#0e459c")),
+          onExit: (PointerEvent event) {
+            homePageNotifier.updateHover();
+            hoverIndex = homePageNotifier.myArray.value.indexOf(0);
+          },
+          child: GestureDetector(
+            onTap: () {
+              zeroIndex = homePageNotifier.myArray.value.indexOf(0);
+
+              if (isAdjacent(index) && isNotDiagonallyOpposite(index)) {
+                lastClicked = index;
+                homePageNotifier.updateArray(
+                    homePageNotifier.myArray.value.indexOf(index), 0);
+                homePageNotifier.updateArray(zeroIndex, index);
+              }
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              child: Center(
+                  child: index != 0
+                      ? Text(
+                          index.toString(),
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 30),
+                        )
+                      : const SizedBox()),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.00),
+                  // border: Border.all(
+                  //   color: Colors.white,
+                  //   width: 3,
+                  // ),
+                  color: index == 0
+                      ? Colors.transparent
+                      : lastClicked == index
+                          ? HexColor("#66b1f1")
+                          : homePageNotifier.isHovering.value &&
+                                  hoverIndex == index
+                              ? hoverColor
+                              : HexColor("#0468d7")),
+            ),
           ),
         ),
       );
--- lib/ValueNotifier/homepage_notifier.dart
+++ lib/ValueNotifier/homepage_notifier.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 class HomePageNotifier with ChangeNotifier {
   ValueNotifier<List<int>> myArray = ValueNotifier([]);
-  int n = 2;
+  int n = 4;
+  ValueNotifier<bool> isHovering = ValueNotifier(false);
 
   void updateArray(int index, int value) {
     myArray.value[index] = value;
     myArray.notifyListeners();
+  }
+
+  void updateHover() {
+    isHovering.value = (!isHovering.value);
+    isHovering.notifyListeners();
   }
 }
 
